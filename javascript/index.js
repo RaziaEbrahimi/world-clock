341ccecdf9efa125e81955091345a4ce663dbb1c
Razia Ebrahimi <razia.ebrahimi706@gmail.com>
2024-10-19 22:27:47 +0430

    display the current date and time of a location

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+function updateTime() {
+  let newyorkElement = document.querySelector("#new-york");
+  let newyorkDateElement = newyorkElement.querySelector(".date");
+  let newyorkTimeElement = newyorkElement.querySelector(".time");
+  let newyorkTime = moment().tz("America/New_York");
+  newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
+  newyorkTimeElement.innerHTML = newyorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+setInterval(updateTime, 1000);
